bc769cb58f3893ad887ffcbc68832c922c054aa7
karacsonyiz <zoltankaracsonyi91@gmail.com>
2021-03-01 15:25:45 +0100

    todos added

--- src/main/java/com/example/jsp/JspApplication.java
+++ src/main/java/com/example/jsp/JspApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 //TODO : refactor ManageController,GeneratedEntity rewrite simple to Entity,Make constants class,Spring security rewrite,
 //TODO : delete unused classes,make Toast,better Session handling,refactor managecontroller,rewrite classnames
+//TODO : spring session,transaction-propagation,optimistic lock,criteriabuilder,lazy load,pagination,auditing from abstract class,
+//TODO : langugage,theme switching,mobile view,carousel,jsp form-formvalidation-rejectvalue,add login password
 public class JspApplication {
 
     public static void main(String[] args) {
